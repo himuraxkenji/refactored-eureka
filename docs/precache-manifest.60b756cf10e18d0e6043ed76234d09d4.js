db0b82d7590039d2d4f194fb8cbf0036ab68a435
himuraxkenji <aaron.ariperto@gmail.com>
2020-07-10 19:00:30 -0300

    docs agregado

---
+++ docs/precache-manifest.60b756cf10e18d0e6043ed76234d09d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1dc305e3d6577928c93f4f965d0059",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fac210fd7d5ba4e73a4",
+    "url": "/static/css/main.7620f449.chunk.css"
+  },
+  {
+    "revision": "16e73fa39235631255e9",
+    "url": "/static/js/2.6518d8e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6518d8e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fac210fd7d5ba4e73a4",
+    "url": "/static/js/main.c6cd13a1.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
