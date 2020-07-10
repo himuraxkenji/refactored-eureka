e52e04ab6e2a4c1cafec1b1211d5cc11814e9818
himuraxkenji <aaron.ariperto@gmail.com>
2020-07-10 18:58:41 -0300

    First Commit

--- README.md
+++ README.md
@@ -66,3 +66,9 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+
+
+
+
+API_KEY = deqsJVVGK84aHTsEyhA3tiD3ta89AKar
---
+++ src/GifExpertApp.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import {AddCategory} from './components/AddCategory';
+import {GifGrid} from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+  const [categorias, setCategorias] = useState(['One Punch']);
+
+
+  // const handleAdd = () => {
+  //   // setCategorias([...categorias, 'HunterXHunter']);
+  //   setCategorias(cats => [...cats, 'HunterXHunter']);
+  // }
+
+
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategorias} />
+      <hr />
+
+      <ol>
+        {
+          categorias.map(categoria => (
+            <GifGrid
+              key={categoria}
+              category={categoria}
+            />
+          ))
+        }
+      </ol>
+    </>
+
+  );
+};
+
